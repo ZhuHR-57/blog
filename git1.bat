257d7f23741b609a7b3c24b4701265a4e51b5ed5
Lido <zhuhr_57@yeah.net>
2023-05-22 20:21:18 +0800

    Auto commit

---
+++ git1.bat
@@ -0,0 +1,12 @@
+@echo off
+@title bat Ωªª•÷¥––git√¸¡Ó
+cd /d F:
+echo "1"
+cd F:/Blog/Blog_Home_New
+echo "2"
+git add .
+echo "3"
+git commit -m "common update"
+echo "4"
+git push -u origin master
+echo "5"
